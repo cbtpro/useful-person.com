2b185d3fbcb9efb6f0f9f31c43797208f7439cba
chenbitao <cbtpro@gmail.com>
2019-08-11 16:55:46 +0800

    增加国际化支持

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,5 @@
+import i18n from "i18next";
+import { useTranslation } from 'react-i18next';
 import Link from 'next/link';
 import Logo from './Logo.js'
 import UserInfo from './UserInfo.js'
@@ -6,28 +8,34 @@ const headerStyle = {
     padding: 15,
     display: 'table',
     width: '100%'
-}
+};
 const linkStyle = {
     marginRight: 15,
     verticalAlign: 'middle',
     textAlign: 'center',
     display: 'table-cell'
 };
-
-const Header = () => (
-    <div className="header" style={headerStyle}>
+const Header = () => {
+    const { t } = useTranslation();
+    return (<div className="header" style={headerStyle}>
         <Logo />
         <Link href="/">
-            <a style={linkStyle} title="首页">首页</a>
+            <a style={linkStyle} title="首页">{t('home')}</a>
         </Link>
         <Link href="/about">
-            <a style={linkStyle} title="关于我们">关于我们</a>
+            <a style={linkStyle} title="关于我们">{t('about us')}</a>
         </Link>
         <Link href="/joinUs">
-            <a style={linkStyle} title="加入我们，你不看一下吗？">加入我们</a>
+            <a style={linkStyle} title={t('join us tip')}>{t('join us')}</a>
         </Link>
+        <select onChange={($event) => {
+            i18n.changeLanguage($event.target.value)
+        }}>
+            <option value="zh">中文</option>
+            <option value="en">English</option>
+        </select>
         <UserInfo />
-    </div>
-);
+    </div>)
+};
 
 export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,43 @@
-import '../i18n.js';
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
 import Layout from '../components/Layout.js';
 
-const { setConfig } = require('next/config')
-setConfig(require('../next.config'))
+const resources = {
+    zh: {
+        translation: {
+            'welcome': '欢迎使用 React & react-i18next.',
+            'home': '首页',
+            'about us': '关于我们',
+            'join us': '加入我们',
+            'join us tip': '加入我们，你不进来看一下吗？！！'
+        }
+    },
+    en: {
+        translation: {
+            "welcome": "Welcome to React and react-i18next",
+            'home': 'Home',
+            'about us': 'About Us',
+            'join us': 'Join Us',
+            'join us tip': 'Join Us, Do you want come in looking for something？！！'
+        }
+    }
+};
 
-const Page = () => <p>Hello Next.js</p>;
+// initialize i18next with catalog and language to use
+i18n
+.use(initReactI18next) // passes i18n down to react-i18next
+.init({
+    resources,
+    lng: 'zh',
+    fallbackLng: "en",
+    interpolation: {
+        escapeValue: false
+    }
+});
+
+const Page = () => {
+    const { t } = useTranslation();
+    return (<p>{t('welcome')}</p>)
+};
 
 export default Layout(Page);
