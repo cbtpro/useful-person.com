24287fb19a4dfafae525dbc3bf7a838e5d460289
陈碧滔 <peter.chen@useful-person.com>
2020-04-04 17:57:09 +0800

    truncate repo

--- pages/index.js
+++
@@ -1,5 +0,0 @@
-import Layout from '../components/Layout.js';
-
-const Page = () => <p>Hello Next.js</p>;
-
-export default Layout(Page);
--- pages/joinUs.js
+++
@@ -1,5 +0,0 @@
-import Layout from '../components/Layout.js';
-
-const Page = () => <p>This is Join Us pages.</p>;
-
-export default Layout(Page);
