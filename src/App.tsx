b46f9fbeddda74f292f0afd1e240d4e6ae6046f4
陈碧滔 <peter.chen@useful-person.com>
2020-04-04 22:24:20 +0800

    添加@typesstyled-jsx支持

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import { Button, DatePicker } from 'antd'
+import B from './B'
+
 import './App.scss'
+
+const helloStyle = { color: 'red' }
+interface A<T> {
+  result: T
+}
 
 const App = () => {
   return <div>
-    <p style={{ color: 'red' }}>Hello world!</p>
-    <p className="some-class-sayWhat">Hello world!</p>
+    <p style={helloStyle}>Hello world!</p>
+    <p className="some-class-say-what">Hello world!</p>
+    <B />
     <DatePicker />
     <Button>你好</Button>
+    <style jsx>{`
+      p {
+        font-size: 36px;
+      }
+    `}</style>
   </div>
 }
 
---
+++ src/B.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const B = () => {
+  return <div>
+    <p>Hello</p>
+    <style jsx>{`
+      p {
+        color: green;
+        font-size: 24px;
+      }
+    `}</style>
+  </div>
+}
+
+export default B
