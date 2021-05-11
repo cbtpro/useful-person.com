e4ffdf6bb404b2ba8c520f18985647bc5afa2c82
PeterChen <cbtpro@gmail.com>
2021-05-11 21:11:29 +0800

    删除无用的代码

--- pages/joinUs.js
+++
@@ -1,5 +0,0 @@
-import Layout from '../components/Layout.js';
-
-const Page = () => <p className="2121">功能正在开发中，敬请期待。</p>;
-
-export default Layout(Page);
--- reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import config from './config';
-import tasks from './tasks';
-import visiblityFilter from './visibilityFilter';
-
-const userfulPersionApp = combineReducers({ config, tasks, visiblityFilter })
-
-export default userfulPersionApp;
