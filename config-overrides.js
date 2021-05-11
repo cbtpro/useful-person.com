74440890a0f280312264ff75fbd4faed30b68adb
PeterChen <cbtpro@gmail.com>
2021-05-11 21:44:38 +0800

    给开发测试环境添加vConsole

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,7 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra')
+const { override, fixBabelImports, addLessLoader, addWebpackAlias, addWebpackPlugin } = require('customize-cra')
+const path = require('path')
+const vConsolePlugin = require('vconsole-webpack-plugin')
+const isEnvProduction = process.env.NODE_ENV === 'production'
 
 module.exports = override(
   fixBabelImports('import', {
@@ -11,5 +14,14 @@ module.exports = override(
     modifyvars: {
       // '@primary-color': '#1DA57A',
     }
-  })
+  }),
+  addWebpackAlias({
+    '@': path.resolve('./src')
+  }),
+  addWebpackPlugin(
+    new vConsolePlugin({
+        filter: [],  // 需要过滤的入口文件
+        enable: !isEnvProduction // 发布代码前记得改回 false
+    }),
+  ),
 )
